e2ebcb78a0c0995c7cded40b471aebdd59b9f550
leonifrancopaz <105676137+leonifrancopaz@users.noreply.github.com>
2022-06-07 03:55:08 -0300

    Update cookie_clicker_pages.py

--- cookie_clicker_pages.py
+++ cookie_clicker_pages.py
@@ -1,5 +1,9 @@
 from js import document
 from pyodide import create_proxy
+import cookie_clicker
 
 def decodef(*args, **kwargs):
-    document.getElementById("decoded-save-code").value = "ayylmao"
+    save_code = document.getElementById("save-code").value
+    save_code = cookie_clicker.decode(save_code)
+    save_code = cookie_clicker.uncompress(save_code)
+    document.getElementById("decoded-save-code").value = save_code
